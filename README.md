b5d1df46ea85a47d1c81626d05276f5c8697dd7f
earl-nicholson <earl.r.nicholson@gmail.com>
2022-04-13 13:21:27 -0500

    reste button

--- README.md
+++ README.md
@@ -19,13 +19,20 @@ This project will challenge your knowledge of React but should also be an exerci
 // assign p1 = O, p2 = X
 
 
-- As a user, my partner should be able to click on a square after me and see their mark.
+- As a user, my partner should be able to click on a square after me and see their mark.(done)
 
 //
 
 
-- As a user, I shouldn't be able to click on a square that has already been selected.
+- As a user, I shouldn't be able to click on a square that has already been selected.(done)
+
+
 - As a user, when someone has won the game (3 squares in a row: horizontally, vertically, or diagonally) I should see a notice telling me who won.
+
+[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]
+
+
+
 - As a user, I should not be able to continue playing the game after the game has been won.
 - As a user, if there are no more squares available, I should see a notice telling me that the game has ended.
 - As a user, I should see a restart button that when clicked will clear the gameboard.
--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     this.state = {
       squares: [null, null, null, null, null, null, null, null, null],
       player1 : '❌',
+      winner : null
     }
   }
 
@@ -17,17 +18,60 @@ click = (index) =>{
    squares[index] = player1
    this.setState({squares: squares, player1: player1 === "❌"?"⭕️":"❌"})
  }
-
 }
-  //   fillP1 = (item) => {
-  //   this.setState({p1: [...this.state.p1, item]})
-  // }
+
+winning = () => {
+  const winningGame= [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+  winningGame.forEach(value => {
+    const [one, two, three] = value
+    const {squares, winner} = this.state
+
+    if (squares[one] === '❌' && squares[two] === '❌' && squares[three] === '❌') {
+      this.setState({ winner: '❌' })
+    } else if (squares[one] === "⭕️" && squares[two] === "⭕️" && squares[three] === "⭕️") {
+      this.setState({ winner: "⭕️" })
+    }
+
+  });
+}
+
+resetGame = () => {
+  this.setState ({
+    squares: [null, null, null, null, null, null, null, null, null],
+    player1 : '❌',
+    winner : null
+  })
+}
+
+
+
+
   render() {
+    const tieGame = this.state.squares.every(value => value !== null)
     console.log(this.state.squares);
     return (
       <>
         <>
         <h1>Tic Tac Toe</h1>
+        {this.state.winner && <h4>{this.state.winner} Wins </h4>}
+        {(tieGame && !this.state.winner) && <h4>Tie</h4>}
+        {(this.state.winner || tieGame) &&
+          <div className = "button">
+            <button onClick = {this.resetGame}>
+                Reset the game
+            </button>
+          </div>
+        }
 
         <div className="gameboard">
           {this.state.squares.map((value, index) => {
